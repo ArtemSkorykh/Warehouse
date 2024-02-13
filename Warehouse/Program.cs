37697b9ee0670314c32f2ee57bb9376841fa4e08
Artem <Skor_v5xz@student.itstep.org>
2024-02-13 19:38:09 +0100

    Модуль 2. Частина 1

--- Warehouse/Program.cs
+++ Warehouse/Program.cs
@@ -11,11 +11,291 @@ namespace Warehouse
         public static string StrConn => @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Warehouse;Integrated Security=True;Connect Timeout=30;";
         private SqlConnection _conn;
 
+        /// Модуль 2. Частина 1
+
+        void DisplayOfAllProducts()
+        {
+            Console.Clear();
+            using (SqlConnection conn = new SqlConnection(StrConn))
+            {
+                conn.Open();
+                var query = "SELECT * FROM Products";
+                var cmd = new SqlCommand(query, conn);
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader["Id"]} {reader["Name"]} {reader["Type"]} {reader["Quantity"]} {reader["PrimeCost"]} {reader["DateOfDelivery"]}");
+                    }
+
+                }
+
+                conn.Close();
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+
+        void DisplayOfAllTypes()
+        {
+            Console.Clear();
+            using (SqlConnection conn = new SqlConnection(StrConn))
+            {
+                conn.Open();
+                var query = "SELECT DISTINCT Products.Type FROM Products";
+                var cmd = new SqlCommand(query, conn);
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader["Type"]}");
+                    }
+
+                }
+
+                conn.Close();
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+
+        void DisplayOfAllManufactures()
+        {
+            Console.Clear();
+            using (SqlConnection conn = new SqlConnection(StrConn))
+            {
+                conn.Open();
+                var query = "SELECT * FROM Manufacturers";
+                var cmd = new SqlCommand(query, conn);
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader["Id"]} {reader["Name"]}");
+                    }
+
+                }
+
+                conn.Close();
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+
+        void DisplayProductWithMaximumQuantity()
+        {
+            Console.Clear();
+            using (SqlConnection conn = new SqlConnection(StrConn))
+            {
+                conn.Open();
+                var query = "SELECT TOP 1  * FROM Products ORDER BY Products.Quantity DESC";
+                var cmd = new SqlCommand(query, conn);
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader["Id"]} {reader["Name"]} {reader["Type"]} {reader["Quantity"]} {reader["PrimeCost"]} {reader["DateOfDelivery"]}");
+                    }
+
+                }
+
+                conn.Close();
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+
+        void DisplayProductWithMinimymQuantity()
+        {
+            Console.Clear();
+            using (SqlConnection conn = new SqlConnection(StrConn))
+            {
+                conn.Open();
+                var query = "SELECT TOP 1  * FROM Products ORDER BY Products.Quantity";
+                var cmd = new SqlCommand(query, conn);
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader["Id"]} {reader["Name"]} {reader["Type"]} {reader["Quantity"]} {reader["PrimeCost"]} {reader["DateOfDelivery"]}");
+                    }
+
+                }
+
+                conn.Close();
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+
+        void DisplayProductWithMaximumCost()
+        {
+            Console.Clear();
+            using (SqlConnection conn = new SqlConnection(StrConn))
+            {
+                conn.Open();
+                var query = "SELECT TOP 1  * FROM Products ORDER BY Products.PrimeCost DESC";
+                var cmd = new SqlCommand(query, conn);
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader["Id"]} {reader["Name"]} {reader["Type"]} {reader["Quantity"]} {reader["PrimeCost"]} {reader["DateOfDelivery"]}");
+                    }
+
+                }
+
+                conn.Close();
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+
+        void DisplayProductWithMinimumCost()
+        {
+            Console.Clear();
+            using (SqlConnection conn = new SqlConnection(StrConn))
+            {
+                conn.Open();
+                var query = "SELECT TOP 1  * FROM Products ORDER BY Products.PrimeCost";
+                var cmd = new SqlCommand(query, conn);
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader["Id"]} {reader["Name"]} {reader["Type"]} {reader["Quantity"]} {reader["PrimeCost"]} {reader["DateOfDelivery"]}");
+                    }
+
+                }
+
+                conn.Close();
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+
+        void DisplayProductsOfGivenType()
+        {
+            Console.Clear();
+
+            string str;
+            Console.Write("Enter the type: ");
+            str = Console.ReadLine();
+
+            Console.WriteLine();
 
 
+            using (SqlConnection conn = new SqlConnection(StrConn))
+            {
+                conn.Open();
+                var query = "SELECT * FROM Products WHERE Products.Type = @str";
+                var cmd = new SqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue("@str", str);
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader["Id"]} {reader["Name"]} {reader["Type"]} {reader["Quantity"]} {reader["PrimeCost"]} {reader["DateOfDelivery"]}");
+                    }
+
+                }
+
+                conn.Close();
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+
+        void DisplayProductsOfGivenManufacturer()
+        {
+            Console.Clear();
+
+            int i;
+            Console.Write("Enter the Manufacturer id: ");
+            i =int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
 
 
+            using (SqlConnection conn = new SqlConnection(StrConn))
+            {
+                conn.Open();
+                var query = "SELECT Products.Name, Products.Type, Products.Quantity, Products.PrimeCost, Products.DateOfDelivery FROM Products, Manufacturers WHERE Products.IdManufaterer = @i";
+                var cmd = new SqlCommand(query, conn);
 
+                cmd.Parameters.AddWithValue("@i", i);
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader["Id"]} {reader["Name"]} {reader["Type"]} {reader["Quantity"]} {reader["PrimeCost"]} {reader["DateOfDelivery"]}");
+                    }
+
+                }
+
+                conn.Close();
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+
+        void DisplayTheProductInStockTheLongestOfAll()
+        {
+            Console.Clear();
+            using (SqlConnection conn = new SqlConnection(StrConn))
+            {
+                conn.Open();
+                var query = "SELECT TOP 1  * FROM Products ORDER BY Products.DateOfDelivery ";
+                var cmd = new SqlCommand(query, conn);
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader["Id"]} {reader["Name"]} {reader["Type"]} {reader["Quantity"]} {reader["PrimeCost"]} {reader["DateOfDelivery"]}");
+                    }
+
+                }
+
+                conn.Close();
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+
+        void DisplayTheAverageNumberOfProductsForEachType()
+        {
+            Console.Clear();
+            using (SqlConnection conn = new SqlConnection(StrConn))
+            {
+                conn.Open();
+                var query = "SELECT Products.Type, AVG(Products.Quantity) AS 'AverageQuantity' FROM Products GROUP BY Products.Type;";
+                var cmd = new SqlCommand(query, conn);
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader["Type"]} {reader["AverageQuantity"]}");
+                    }
+
+                }
+
+                conn.Close();
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
 
         static void Main(string[] args)
         {
@@ -51,68 +331,60 @@ namespace Warehouse
                     int j = -1;
                     while (j != 0)
                     {
+                        /// Модуль 2.Частина 1
                         Console.WriteLine("Enter the comand \n1 - Display All Information About Products           | 2 - Display All Types Of Products              | 3 - Display All Manufactures");
                         Console.WriteLine("4 -  Display The Product With Maximum Quantity       | 5 -  Display The Product With Minimum Quntity  | 6 - Display The Product With Minimal Cost");
                         Console.WriteLine("7 -  Display The Product With Maximum Cost           | 8 -  Display Products Of Given Type            | 9 - Display Products Of Given Manufacturer");
                         Console.WriteLine("10 - Display The Product In Stock The Longest Of All | 11 - Display The Average Number Of Products For Each Type");
+                        /// Модуль 2.Частина 2
                         Console.WriteLine("12 - Insert New Information                          | 13 - Delete Information                        | 14 - Update Information");
                         Console.WriteLine("0 - Exit");
 
                         Console.Write("Comand: ");
                         j = Int32.Parse(Console.ReadLine());
-                        //switch (j)
-                        //{
-                        //    case 1:
-                        //        Program.DisplayOfAllInformation();
-                        //        break;
-                        //    case 2:
-                        //        Program.DisplayOfAllNames();
-                        //        break;
-                        //    case 3:
-                        //        Program.DisplayAllColors();
-                        //        break;
-                        //    case 4:
-                        //        Program.DisplayTheMaximumCalorie();
-                        //        break;
-                        //    case 5:
-                        //        Program.DisplayTheMinimumCalorie();
-                        //        break;
-                        //    case 6:
-                        //        Program.DisplayTheAverageCalorie();
-                        //        break;
-                        //    case 7:
-                        //        Program.DisplayNumberOfVegetables();
-                        //        break;
-                        //    case 8:
-                        //        Program.DisplayNumberOfFruits();
-                        //        break;
-                        //    case 9:
-                        //        Program.DisplayNumberOfGivenColor();
-                        //        break;
-                        //    case 10:
-                        //        Program.DisplayNumbersOfEveryColor();
-                        //        break;
-                        //    case 11:
-                        //        Program.DisplayWithCaloriesBelow();
-                        //        break;
-                        //    case 12:
-                        //        Program.DisplayWithCaloriesHigher();
-                        //        break;
-                        //    case 13:
-                        //        Program.DisplayAllYellowOrRedColor();
-                        //        break;
-                        //    case 14:
-                        //        Program.DisplayWithcalorieContentInTheIndicatedRange();
-                        //        break;
-
-                        //    default:
-                        //        j = 0;
-                        //        break;
-                        //}
+                        switch (j)
+                        {
+                            case 1: 
+                                Program.DisplayOfAllProducts();
+                                break;
+                            case 2:
+                                Program.DisplayOfAllTypes();
+                                break;
+                            case 3:
+                                Program.DisplayOfAllManufactures();
+                                break;
+                            case 4:
+                                Program.DisplayProductWithMaximumQuantity();
+                                break;
+                            case 5:
+                                Program.DisplayProductWithMinimymQuantity();
+                                break;
+                            case 6:
+                                Program.DisplayProductWithMinimumCost();      
+                                break;
+                            case 7:
+                                Program.DisplayProductWithMaximumCost();
+                                break;
+                            case 8:
+                                Program.DisplayProductsOfGivenType();
+                                break;
+                            case 9:
+                                Program.DisplayProductsOfGivenManufacturer();
+                                break;
+                            case 10:
+                                Program.DisplayTheProductInStockTheLongestOfAll();
+                                break;
+                            case 11:
+                                Program.DisplayTheAverageNumberOfProductsForEachType();
+                                break;
+                            default:
+                                j = 0;
+                                break;
+                        }
                     }
                     i = -1;
                 }
-            }
+            }                                                                                                                                                                                                                                                                                                                                                                                                                                        
         }
     }
 }
